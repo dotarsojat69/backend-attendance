f8b5091a7c36b1083322024809e614b9a8cb0eda
dotarsojat69 <tdeath457@gmail.com>
2024-08-03 21:20:27 +0700

    feat: add new routes for id user

--- src/users/users.routes.ts
+++ src/users/users.routes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { userGet, userUpdate, userDelete } from "./users.controller";
+import { userGet, userUpdate, userDelete, userGetDetail } from "./users.controller";
 import { verifyToken } from "../utils/middlewares/auth";
 import upload from "../utils/configs/multer";
 
@@ -9,17 +9,29 @@ const router = express.Router();
 router
   .route("/users")
   .get(
-    (req, res, next) => verifyToken(req, res, next, ["user", "admin"]),
+    (req, res, next) => verifyToken(req, res, next, ["admin"]),
     userGet
+  )
+  .put(
+    (req, res, next) => verifyToken(req, res, next, ["admin"]),
+    upload.single("profile_picture"),
+    userUpdate
+  )
+  .delete(
+    (req, res, next) => verifyToken(req, res, next, ["admin"]),
+    userDelete
+  );
+
+  router
+  .route("/users/:id_user")
+  .get(
+    (req, res, next) => verifyToken(req, res, next, ["user", "admin"]),
+    userGetDetail
   )
   .put(
     (req, res, next) => verifyToken(req, res, next, ["user", "admin"]),
     upload.single("profile_picture"),
     userUpdate
-  )
-  .delete(
-    (req, res, next) => verifyToken(req, res, next, ["user", "admin"]),
-    userDelete
   );
 
 export default router;
